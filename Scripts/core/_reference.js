0b286b538d65bd965e0d781a6cfbb5bd36753a64
namourdon <nash.amourdon@gmail.com>
2016-02-04 13:52:22 -0500

    --

--- Scripts/core/_reference.js
+++ Scripts/core/_reference.js
@@ -3,7 +3,11 @@
 /// <reference path="../objects/label.ts" />
 /// <reference path="../objects/button.ts" />
 /// <reference path="../objects/scene.ts" />
-/// <reference path="../scenes/rightcave.ts" />
-/// <reference path="../scenes/leftcave.ts" />
-/// <reference path="../scenes/intro.ts" /> 
+/// <reference path="../scenes/mainscreen.ts" />
+/// <reference path="../scenes/prince.ts" />
+/// <reference path="../scenes/warrior.ts" />
+///<reference path="../scenes/princedoor.ts"/>
+///<reference path="../scenes/princekey.ts"/>
+///<reference path="../scenes/warriordoor.ts"/>
+///<reference path="../scenes/warriorkey.ts"/> 
 //# sourceMappingURL=_reference.js.map
